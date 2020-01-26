c2e88e6a66653fe05aa664c96ad1c283c4e30de5
yaser hani timim <yaserht@uia.no>
2020-01-26 20:45:44 +0100

    i have added assignment_1_1

---
+++ assignment_1_1/main.py
@@ -0,0 +1,26 @@
+for i in range(1,11):
+    print(i,end=' ')
+print("\n")
+for j in range(2,22,2):
+    print(j,end=' ')
+print("\n")
+for k in range(1,20,2):
+    print(k,end=' ')
+print("\n")
+for l in range(1,50,3):
+    print(l,end=' ')
+print("\n")
+for m in range(40,1,-4):
+    print(m,end=' ')
+print("\n")
+for Number in range (1, 101):
+    count = 0
+    for i in range(2, (Number//2 + 1)):
+        if(Number % i == 0):
+            count = count + 1
+            break
+
+    if (count == 0 and Number != 1):
+        print(" %d" %Number, end = '')
+
+
